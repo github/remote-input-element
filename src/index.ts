5075ffb2c733951e87b2191e7598e39419373b36
Tyler Jones <tylerjdev@github.com>
2024-03-29 20:04:25 +0000

    Update lint

--- src/index.ts
+++ src/index.ts
@@ -1,167 +1,192 @@
-const states = new WeakMap()
+const states = new WeakMap();
 
 class RemoteInputElement extends HTMLElement {
   constructor() {
-    super()
-    const fetch = fetchResults.bind(null, this, true)
-    const state = {currentQuery: null, oninput: debounce((e: Event) => fetch(e)), fetch, controller: null}
-    states.set(this, state)
+    super();
+    const fetch = fetchResults.bind(null, this, true);
+    const state = {
+      currentQuery: null,
+      oninput: debounce<Event>((e) => fetch(e)),
+      fetch,
+      controller: null,
+    };
+    states.set(this, state);
   }
 
   static get observedAttributes() {
-    return ['src']
+    return ["src"];
   }
 
   attributeChangedCallback(name: string, oldValue: string) {
-    if (oldValue && name === 'src') {
-      fetchResults(this, false)
+    if (oldValue && name === "src") {
+      fetchResults(this, false);
     }
   }
 
   connectedCallback() {
-    const input = this.input
-    if (!input) return
+    const input = this.input;
+    if (!input) return;
 
-    input.setAttribute('autocomplete', 'off')
-    input.setAttribute('spellcheck', 'false')
+    input.setAttribute("autocomplete", "off");
+    input.setAttribute("spellcheck", "false");
 
-    const state = states.get(this)
-    if (!state) return
+    const state = states.get(this);
+    if (!state) return;
 
-    input.addEventListener('focus', state.fetch)
-    input.addEventListener('change', state.fetch)
-    input.addEventListener('input', state.oninput)
+    input.addEventListener("focus", state.fetch);
+    input.addEventListener("change", state.fetch);
+    input.addEventListener("input", state.oninput);
   }
 
   disconnectedCallback() {
-    const input = this.input
-    if (!input) return
+    const input = this.input;
+    if (!input) return;
 
-    const state = states.get(this)
-    if (!state) return
+    const state = states.get(this);
+    if (!state) return;
 
-    input.removeEventListener('focus', state.fetch)
-    input.removeEventListener('change', state.fetch)
-    input.removeEventListener('input', state.oninput)
+    input.removeEventListener("focus", state.fetch);
+    input.removeEventListener("change", state.fetch);
+    input.removeEventListener("input", state.oninput);
   }
 
   get input(): HTMLInputElement | HTMLTextAreaElement | null {
-    const input = this.querySelector('input, textarea')
-    return input instanceof HTMLInputElement || input instanceof HTMLTextAreaElement ? input : null
+    const input = this.querySelector("input, textarea");
+    return input instanceof HTMLInputElement ||
+      input instanceof HTMLTextAreaElement
+      ? input
+      : null;
   }
 
   get src(): string {
-    return this.getAttribute('src') || ''
+    return this.getAttribute("src") || "";
   }
 
   set src(url: string) {
-    this.setAttribute('src', url)
+    this.setAttribute("src", url);
   }
 }
 
 function makeAbortController() {
-  if ('AbortController' in window) {
-    return new AbortController()
+  if ("AbortController" in window) {
+    return new AbortController();
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  return {signal: null, abort() {}}
+  return { signal: null, abort() {} };
 }
 
-async function fetchResults(remoteInput: RemoteInputElement, checkCurrentQuery: boolean, event?: Event) {
-  const input = remoteInput.input
-  if (!input) return
+async function fetchResults(
+  remoteInput: RemoteInputElement,
+  checkCurrentQuery: boolean,
+  event?: Event,
+) {
+  const input = remoteInput.input;
+  if (!input) return;
 
-  const state = states.get(remoteInput)
-  if (!state) return
+  const state = states.get(remoteInput);
+  if (!state) return;
 
-  const query = input.value
-  if (checkCurrentQuery && state.currentQuery === query) return
+  const query = input.value;
+  if (checkCurrentQuery && state.currentQuery === query) return;
 
-  state.currentQuery = query
+  state.currentQuery = query;
 
-  const src = remoteInput.src
-  if (!src) return
+  const src = remoteInput.src;
+  if (!src) return;
 
-  const resultsContainer = document.getElementById(remoteInput.getAttribute('aria-owns') || '')
-  if (!resultsContainer) return
+  const resultsContainer = document.getElementById(
+    remoteInput.getAttribute("aria-owns") || "",
+  );
+  if (!resultsContainer) return;
 
-  const url = new URL(src, window.location.href)
-  const params = new URLSearchParams(url.search)
-  params.append(remoteInput.getAttribute('param') || 'q', query)
-  url.search = params.toString()
+  const url = new URL(src, window.location.href);
+  const params = new URLSearchParams(url.search);
+  params.append(remoteInput.getAttribute("param") || "q", query);
+  url.search = params.toString();
 
   if (state.controller) {
-    state.controller.abort()
+    state.controller.abort();
   } else {
-    remoteInput.dispatchEvent(new CustomEvent('loadstart'))
-    remoteInput.setAttribute('loading', '')
+    remoteInput.dispatchEvent(new CustomEvent("loadstart"));
+    remoteInput.setAttribute("loading", "");
   }
 
-  state.controller = makeAbortController()
+  state.controller = makeAbortController();
 
-  let response
-  let html = ''
+  let response;
+  let html = "";
   try {
     response = await fetchWithNetworkEvents(remoteInput, url.toString(), {
       signal: state.controller.signal,
-      credentials: 'same-origin',
-      headers: {accept: 'text/fragment+html'}
-    })
-    html = await response.text()
-    remoteInput.removeAttribute('loading')
-    state.controller = null
+      credentials: "same-origin",
+      headers: { accept: "text/fragment+html" },
+    });
+    html = await response.text();
+    remoteInput.removeAttribute("loading");
+    state.controller = null;
   } catch (error) {
-    if (error instanceof Error && error.name !== 'AbortError') {
-      remoteInput.removeAttribute('loading')
-      state.controller = null
+    if (error instanceof Error && error.name !== "AbortError") {
+      remoteInput.removeAttribute("loading");
+      state.controller = null;
     }
-    return
+    return;
   }
 
   if (response && response.ok) {
-    resultsContainer.innerHTML = html
-    remoteInput.dispatchEvent(new CustomEvent('remote-input-success', {bubbles: true, detail: { "eventType": event? event.type : undefined}}))
+    resultsContainer.innerHTML = html;
+    remoteInput.dispatchEvent(
+      new CustomEvent("remote-input-success", {
+        bubbles: true,
+        detail: { eventType: event ? event.type : undefined },
+      }),
+    );
   } else {
-    remoteInput.dispatchEvent(new CustomEvent('remote-input-error', {bubbles: true}))
+    remoteInput.dispatchEvent(
+      new CustomEvent("remote-input-error", { bubbles: true }),
+    );
   }
 }
 
-async function fetchWithNetworkEvents(el: Element, url: string, options: RequestInit): Promise<Response> {
+async function fetchWithNetworkEvents(
+  el: Element,
+  url: string,
+  options: RequestInit,
+): Promise<Response> {
   try {
-    const response = await fetch(url, options)
-    el.dispatchEvent(new CustomEvent('load'))
-    el.dispatchEvent(new CustomEvent('loadend'))
-    return response
+    const response = await fetch(url, options);
+    el.dispatchEvent(new CustomEvent("load"));
+    el.dispatchEvent(new CustomEvent("loadend"));
+    return response;
   } catch (error) {
-    if (error instanceof Error && error?.name !== 'AbortError') {
-      el.dispatchEvent(new CustomEvent('error'))
-      el.dispatchEvent(new CustomEvent('loadend'))
+    if (error instanceof Error && error?.name !== "AbortError") {
+      el.dispatchEvent(new CustomEvent("error"));
+      el.dispatchEvent(new CustomEvent("loadend"));
     }
-    throw error
+    throw error;
   }
 }
 
-function debounce<T>(callback: (args: T) => void) {
-  let timeout: ReturnType<typeof setTimeout>
-  return function(args: T) {
-    clearTimeout(timeout)
+function debounce<T>(callback: (_args: T) => void) {
+  let timeout: ReturnType<typeof setTimeout>;
+  return function (args: T) {
+    clearTimeout(timeout);
     timeout = setTimeout(() => {
-      clearTimeout(timeout)
-      callback(args)
-    }, 300)
-  }
+      clearTimeout(timeout);
+      callback(args);
+    }, 300);
+  };
 }
 
-export default RemoteInputElement
+export default RemoteInputElement;
 
 declare global {
+  // eslint-disable-next-line no-unused-vars
   interface Window {
-    RemoteInputElement: typeof RemoteInputElement
+    RemoteInputElement: typeof RemoteInputElement;
   }
 }
 
-if (!window.customElements.get('remote-input')) {
-  window.RemoteInputElement = RemoteInputElement
-  window.customElements.define('remote-input', RemoteInputElement)
+if (!window.customElements.get("remote-input")) {
+  window.RemoteInputElement = RemoteInputElement;
+  window.customElements.define("remote-input", RemoteInputElement);
 }
--- test/test.js
+++ test/test.js
@@ -1,155 +1,175 @@
-describe('remote-input', function() {
-  describe('element creation', function() {
-    it('creates from document.createElement', function() {
-      const el = document.createElement('remote-input')
-      assert.equal('REMOTE-INPUT', el.nodeName)
-    })
+describe("remote-input", function () {
+  describe("element creation", function () {
+    it("creates from document.createElement", function () {
+      const el = document.createElement("remote-input");
+      assert.equal("REMOTE-INPUT", el.nodeName);
+    });
 
-    it('creates from constructor', function() {
-      const el = new window.RemoteInputElement()
-      assert.equal('REMOTE-INPUT', el.nodeName)
-    })
-  })
+    it("creates from constructor", function () {
+      const el = new window.RemoteInputElement();
+      assert.equal("REMOTE-INPUT", el.nodeName);
+    });
+  });
 
-  describe('after tree insertion', function() {
-    let remoteInput
-    let input
-    let results
+  describe("after tree insertion", function () {
+    let remoteInput;
+    let input;
+    let results;
 
-    beforeEach(function() {
+    beforeEach(function () {
       document.body.innerHTML = `
         <remote-input aria-owns="results" src="/results">
           <input>
         </remote-input>
         <div id="results"></div>
-      `
-      remoteInput = document.querySelector('remote-input')
-      input = remoteInput.querySelector('input')
-      results = document.querySelector('#results')
-    })
+      `;
+      remoteInput = document.querySelector("remote-input");
+      input = remoteInput.querySelector("input");
+      results = document.querySelector("#results");
+    });
 
-    afterEach(function() {
-      document.body.innerHTML = ''
-      remoteInput = null
-      input = null
-      results = null
-    })
+    afterEach(function () {
+      document.body.innerHTML = "";
+      remoteInput = null;
+      input = null;
+      results = null;
+    });
 
-    it('emits network events in order', async function() {
-      const events = []
-      const track = event => events.push(event.type)
+    it("emits network events in order", async function () {
+      const events = [];
+      const track = (event) => events.push(event.type);
 
-      remoteInput.addEventListener('loadstart', track)
-      remoteInput.addEventListener('load', track)
-      remoteInput.addEventListener('loadend', track)
+      remoteInput.addEventListener("loadstart", track);
+      remoteInput.addEventListener("load", track);
+      remoteInput.addEventListener("loadend", track);
 
       const completed = Promise.all([
-        once(remoteInput, 'loadstart'),
-        once(remoteInput, 'load'),
-        once(remoteInput, 'loadend')
-      ])
-      changeValue(input, 'test')
-      await completed
+        once(remoteInput, "loadstart"),
+        once(remoteInput, "load"),
+        once(remoteInput, "loadend"),
+      ]);
+      changeValue(input, "test");
+      await completed;
 
-      assert.deepEqual(['loadstart', 'load', 'loadend'], events)
-    })
+      assert.deepEqual(["loadstart", "load", "loadend"], events);
+    });
 
-    it('loads content', async function() {
-      assert.equal(results.innerHTML, '')
+    it("loads content", async function () {
+      assert.equal(results.innerHTML, "");
 
-      const success = once(remoteInput, 'remote-input-success')
-      const loadend = once(remoteInput, 'loadend')
+      const success = once(remoteInput, "remote-input-success");
+      const loadend = once(remoteInput, "loadend");
 
-      changeValue(input, 'test')
+      changeValue(input, "test");
 
-      await success
-      await loadend
-      assert.equal(results.querySelector('ol').getAttribute('data-src'), '/results?q=test')
-    })
+      await success;
+      await loadend;
+      assert.equal(
+        results.querySelector("ol").getAttribute("data-src"),
+        "/results?q=test",
+      );
+    });
 
-    it('handles not ok responses', async function() {
-      remoteInput.src = '/500'
-      assert.equal(results.innerHTML, '')
+    it("handles not ok responses", async function () {
+      remoteInput.src = "/500";
+      assert.equal(results.innerHTML, "");
 
-      const error = once(remoteInput, 'remote-input-error')
-      const loadend = once(remoteInput, 'loadend')
+      const error = once(remoteInput, "remote-input-error");
+      const loadend = once(remoteInput, "loadend");
 
-      changeValue(input, 'test')
+      changeValue(input, "test");
 
-      await loadend
-      await error
+      await loadend;
+      await error;
 
-      assert.equal(results.innerHTML, '', 'nothing was appended')
-    })
+      assert.equal(results.innerHTML, "", "nothing was appended");
+    });
 
-    it('handles network error', async function() {
-      remoteInput.src = '/network-error'
-      assert.equal(results.innerHTML, '')
+    it("handles network error", async function () {
+      remoteInput.src = "/network-error";
+      assert.equal(results.innerHTML, "");
 
-      const result = once(remoteInput, 'error')
+      const result = once(remoteInput, "error");
 
-      changeValue(input, 'test')
-      assert.ok(remoteInput.hasAttribute('loading'), 'loading attribute should have been added')
+      changeValue(input, "test");
+      assert.ok(
+        remoteInput.hasAttribute("loading"),
+        "loading attribute should have been added",
+      );
 
-      await result
-      await nextTick()
-      assert.equal(results.innerHTML, '', 'nothing was appended')
-      assert.notOk(remoteInput.hasAttribute('loading'), 'loading attribute should have been removed')
-    })
+      await result;
+      await nextTick();
+      assert.equal(results.innerHTML, "", "nothing was appended");
+      assert.notOk(
+        remoteInput.hasAttribute("loading"),
+        "loading attribute should have been removed",
+      );
+    });
 
-    it('repects param attribute', async function() {
-      remoteInput.setAttribute('param', 'robot')
-      assert.equal(results.innerHTML, '')
+    it("repects param attribute", async function () {
+      remoteInput.setAttribute("param", "robot");
+      assert.equal(results.innerHTML, "");
 
-      const result = once(remoteInput, 'remote-input-success')
+      const result = once(remoteInput, "remote-input-success");
 
-      changeValue(input, 'test')
+      changeValue(input, "test");
 
-      await result
-      assert.equal(results.querySelector('ol').getAttribute('data-src'), '/results?robot=test')
-    })
+      await result;
+      assert.equal(
+        results.querySelector("ol").getAttribute("data-src"),
+        "/results?robot=test",
+      );
+    });
 
-    it('loads content again after src is changed', async function() {
-      const result1 = once(remoteInput, 'remote-input-success')
-      changeValue(input, 'test')
-      
-      await result1
-      assert.equal(results.querySelector('ol').getAttribute('data-src'), '/results?q=test')
+    it("loads content again after src is changed", async function () {
+      const result1 = once(remoteInput, "remote-input-success");
+      changeValue(input, "test");
 
-      const result2 = once(remoteInput, 'remote-input-success')
-      remoteInput.src = '/srcChanged'
+      await result1;
+      assert.equal(
+        results.querySelector("ol").getAttribute("data-src"),
+        "/results?q=test",
+      );
 
-      await result2
-      assert.equal(results.querySelector('ol').getAttribute('data-src'), '/srcChanged?q=test')
-    })
+      const result2 = once(remoteInput, "remote-input-success");
+      remoteInput.src = "/srcChanged";
 
-    it('check eventType', async function() {
-      const result = new Promise(resolve => {
-        remoteInput.addEventListener('remote-input-success', (event) => {
-          resolve(event)
-        }, {once: true})
-      })
-      changeValue(input, 'test')
+      await result2;
+      assert.equal(
+        results.querySelector("ol").getAttribute("data-src"),
+        "/srcChanged?q=test",
+      );
+    });
 
-      result.then((value) => {
-        assert.equal(value.detail.eventType, 'change')
-      })
-      
-    })
-  })
-})
+    it("check eventType", async function () {
+      const result = new Promise((resolve) => {
+        remoteInput.addEventListener(
+          "remote-input-success",
+          (event) => {
+            resolve(event);
+          },
+          { once: true },
+        );
+      });
+      changeValue(input, "test");
+
+      const value = await result;
+      assert.equal(value.detail.eventType, "change");
+    });
+  });
+});
 
 function changeValue(input, value) {
-  input.value = value
-  input.dispatchEvent(new Event('focus'))
+  input.value = value;
+  input.dispatchEvent(new Event("focus"));
 }
 
 function nextTick() {
-  return Promise.resolve()
+  return Promise.resolve();
 }
 
 function once(element, eventName) {
-  return new Promise(resolve => {
-    element.addEventListener(eventName, resolve, {once: true})
-  })
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, resolve, { once: true });
+  });
 }
